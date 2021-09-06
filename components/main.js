749f3211903ffe5071d52ce26a2d3fe74637318c
eevaain <evanlin416@gmail.com>
2021-09-06 18:00:59 -0400

    added sun, solved responsiveness issues

--- components/main.js
+++ components/main.js
@@ -3,8 +3,8 @@ export default function Main() {
     return(
       <>
       <div className = "flex justify-between">
-        <img className = "h-28 sm: h-20"src="vector.png"></img>
-          <div className = "cursor-pointer rounded-full h-12 w-40 flex items-center justify-center bg-gray-100 filter drop-shadow-xl transform transition duration-400 hover:scale-110 mt-8 sm:w-52 h-14">
+        <img className = "h-24 sm:h-28"src="vector.png"></img>
+          <div className = "cursor-pointer rounded-full h-12 w-40 flex items-center justify-center bg-gray-100 filter drop-shadow-xl transform transition duration-400 hover:scale-110 mt-8 sm:w-52 sm:h-14">
             <a className = "font-bold text-sm text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 sm:text-lg">My Newsletter</a>
           </div>
       </div>
@@ -13,12 +13,13 @@ export default function Main() {
          <a className = "flex justify-center font-extrabold text-5xl text-center pb-2 pt-4 sm:text-6xl"> Hey! I'm Evan.</a>
           <div className = "flex flex-row justify-center">
             <a className = "font-regular text-lg sm:text-3xl">Welcome to my website! </a>
-            <img src="wave.png" className="wave h-8"/>
+            <img src="wave.png" className="wave h-7 sm:h-10"/>
           </div>
       </div>
         <div className = "portfolio">
           <div className = "flex flex-col items-center ">
-            <img className = "flex h-52  justify-center sm:h-72" src="me.png"></img>
+            <div className = "circle fixed rounded-full h-20 w-20 flex items-center justify-center mr-16 bg-yellow-200 filter drop-shadow-lg sm:h-24 sm:w-24 sm:mr-20"></div>
+            <img className = "flex h-52 justify-center z-10 sm:h-72" src="me.png"></img>
             <div className = "cursor-pointer rounded-full py-4 w-72 flex items-center justify-center bg-gray-100 filter drop-shadow-xl transform transition duration-400 hover:scale-110 sm:w-96" onClick = {() => window.open('https://trusting-clutch-140.notion.site/6052530559bc4e1db8a48520d056da80?v=d7765ed8598843e1bd79d17d3187933c')}>
               <a className = "font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">My Portfolio</a>
             </div>
