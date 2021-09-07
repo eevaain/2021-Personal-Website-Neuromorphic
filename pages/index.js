4d9705a8f7043f7385d5bed704479e75ee3cb534
eevaain <evanlin416@gmail.com>
2021-09-07 15:03:41 -0400

    ok

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className = "min-h-screen p-10 flex flex-col dark:bg-gray-900">
       <Head>
-       <title>Evan Lin</title>
+       <title>Evan Lin | Personal Website</title>
        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main/>
