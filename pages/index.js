034bef83f5710f68b7a2aadfa62ee5344561267e
eevaain <evanlin416@gmail.com>
2022-08-26 15:59:32 -0400

    asdsdsa

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Image from 'next/image'
 import Main from '../components/main'
 
 
+
+
+
+
 export default function Home() {
   return (
     <div className = "min-h-screen p-10 flex flex-col bg-white">
