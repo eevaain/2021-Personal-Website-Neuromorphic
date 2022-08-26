5bc7a1ef6ed285db50573d311a66e0adb730e580
eevaain <evanlin416@gmail.com>
2022-08-26 16:21:23 -0400

    Revert "Revert "pogu""
    This reverts commit dca600523d26e92036db3d1db8ddce4c942249a6.

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ import Main from '../components/main'
 
 export default function Home() {
   return (
-    <div className = "min-h-screen p-10 flex flex-col dark:bg-gray-900">
+    <div className = "min-h-screen p-10 flex flex-col bg-white">
       <Head>
-       <title>Evan Lin | Personal Website</title>
+       <title>Evan Lin</title>
        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main/>
