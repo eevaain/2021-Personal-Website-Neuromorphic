d1a5843a698e26dee8b66a02a758c75dfa95eb4c
eevaain <evanlin416@gmail.com>
2022-08-26 16:17:57 -0400

    Revert "asdsdsa"
    This reverts commit 034bef83f5710f68b7a2aadfa62ee5344561267e.

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,6 @@ import Image from 'next/image'
 import Main from '../components/main'
 
 
-
-
-
-
 export default function Home() {
   return (
     <div className = "min-h-screen p-10 flex flex-col dark:bg-gray-900">
